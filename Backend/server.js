55d3a35f991ebb1e43d139fc275735cb6bada7e1
CopyNiinja <killfyz0.o@gmail.com>
2024-10-14 19:43:03 +0600

    checking

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,9 @@
+import cors from 'cors';
 import express from 'express';
-
 const app = express();
+app.use(cors())
 app.get('/',(req,res)=>{
-    res.send("Hello world");
+    res.json({ name:"Faiyaz",age:25});
 })
 app.listen(3000,()=>{
     console.log("The server is on port 3000...");
--- src/Pages/BusPage.jsx
+++ src/Pages/BusPage.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import Nav from "../Components/Nav";
 
 export default function BusPage() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch("http://localhost:3000/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
   return (
     <div>
       <Nav />
       Bus
+      <p>{data?.name}</p>
     </div>
   );
 }
