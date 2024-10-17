31fcae2838500aa8b70f4e496a154de6cbf119a3
CopyNiinja <killfyz0.o@gmail.com>
2024-10-17 14:35:55 +0600

    2

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -2,7 +2,7 @@ import About from "../Components/About";
 
 export default function AboutPage() {
   return (
-    <div className="  flex flex-col">
+    <div className="  flex flex-col justify-between">
       <About />
     </div>
   );
